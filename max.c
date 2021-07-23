b55104e3edd4547563bb19a778dc6ef55e670a30
unknown <achikimakobo17@gmail.com>
2021-07-23 16:31:13 +0300

    Тестирование 1-ой задачи: ф-ии поиска мин. и макс. из 2-х знаечний

---
+++ max.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int max (int a, int b) {
+
+	if (a == b) {
+		printf ("a = b = %d\n", a);
+		return a;
+	}
+    
+	if (a != b) {
+		if (a > b) {
+		return a;
+	}
+	else{
+		return b;
+	}
+	}
+}
--- tst_1.c
+++ tst_1.c
@@ -3,19 +3,50 @@
 #include <setjmp.h>
 #include "cmocka\include\cmocka.h"
 
+ int max (int a, int b);
+ int min (int a, int b);
+ int maxim(int a, int b, int c, int d);
+ int star(int a, int b, int c, int d) ;
 
 static void test_1(void **state) {
-
-    // вот здесь (то есть ниже) вы можете делать все что захотите (в пределах синтаксиса языка Си)
-
-
-
-    // все, заканчиваем... ниже ничего писать не надо
+	
+	int res1 = min(1,4);
+	assert_int_equal (res1, 1);
+}
+static void test_2(void **state) {
+	
+	int res2 = min(6,2);
+	assert_int_equal (res2, 2);
+}
+static void test_3(void **state) {
+	
+	int res3 = min(3,3);
+	assert_int_equal (res3, 3);
+}
+static void test_4(void **state) {
+	
+	int res4 = max(1,4);
+	assert_int_equal (res4, 4);
+}
+static void test_5(void **state) {
+	
+	int res5 = max(6,2);
+	assert_int_equal (res5, 6);
+}
+static void test_6(void **state) {
+	
+	int res6 = max(3,3);
+	assert_int_equal (res6, 3);
 }
 
 int main(void) {
     const struct CMUnitTest tests[] = {
         cmocka_unit_test( test_1 ),
+		cmocka_unit_test( test_2 ),
+		cmocka_unit_test( test_3 ),
+		cmocka_unit_test( test_4 ),
+		cmocka_unit_test( test_5 ),
+		cmocka_unit_test( test_6 ),
     };
     return cmocka_run_group_tests(tests, NULL, NULL);
 }
